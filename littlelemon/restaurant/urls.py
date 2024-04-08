aec27144e62908bdc04e5d4536551dee978a0a49
alass1996git <alass1996@gmail.com>
2024-04-07 20:20:36 -0700

    adding basic restful APIs

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin 
 from django.urls import path 
 from . import views
-  
+from rest_framework import routers
+
 urlpatterns = [ 
-    #path('', views.sayHello, name='sayHello'), 
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
